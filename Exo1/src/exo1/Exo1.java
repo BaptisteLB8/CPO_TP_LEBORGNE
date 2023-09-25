bc0115265a0c9f85336f74ff4a37b00615a34eaf
bapti <bapti@LAPTOP-OARLVC8V.education.lakanal.epf.fr>
2023-09-25 12:58:37 +0200

    Exo1

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,33 @@
+/*
+* TP0 Exercice 1
+* LE BORGNE TDA 
+* Septembre 2023 
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bapti
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    System.out.println("Bonjour");
+    System.out.print("Ca va ?");
+    System.out.print("au revoir ");
+        int i = 20;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre prenom ?");  
+        String prenom = sc.nextLine();
+System.out.print("Bonjour " + prenom + " au revoir ");
+// TODO code application logic here
+    }
+    
+}
