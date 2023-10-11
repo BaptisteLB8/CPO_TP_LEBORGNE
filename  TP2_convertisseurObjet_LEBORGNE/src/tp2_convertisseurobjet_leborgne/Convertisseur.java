e85b2159dee347183063c9b3040574c8c5a8eb36
bapti <bapti@LAPTOP-OARLVC8V.education.lakanal.epf.fr>
2023-10-11 14:40:18 +0200

    TP2.2

---
+++  TP2_convertisseurObjet_LEBORGNE/src/tp2_convertisseurobjet_leborgne/Convertisseur.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_leborgne;
+
+/**
+ *
+ * @author bapti
+ */
+class Convertisseur {
+
+    double KelvinVersCelcius(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    double KelvinVersFarenheit(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    double CelciusVersKelvin(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    double CelciusVersFarenheit(double d) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---  TP2_convertisseurObjet_LEBORGNE/src/tp2_convertisseurobjet_leborgne/TP2_convertisseurObjet_LEBORGNE.java
+++  TP2_convertisseurObjet_LEBORGNE/src/tp2_convertisseurobjet_leborgne/TP2_convertisseurObjet_LEBORGNE.java
@@ -14,7 +14,21 @@ public class TP2_convertisseurObjet_LEBORGNE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
+        Convertisseur nbConversions  = new Convertisseur();
+
+        double tKelvin1 = nbConversions.CelciusVersKelvin(25.0);
+        double tFarenheit1 = nbConversions .CelciusVersFarenheit(30.0);
+
+        System.out.println("Nombre de conversions effectuées par le convertisseur1 : " + nbConversions );
+
+        Convertisseur convertisseur2 = new Convertisseur();
+
+        double tCelcius2 = convertisseur2.KelvinVersCelcius(298.15);
+        double tFarenheit2 = convertisseur2.KelvinVersFarenheit(275.0);
+
+        System.out.println("Nombre de conversions effectuées par le convertisseur2 : " + convertisseur2);
     }
-    
 }
+   
+    
