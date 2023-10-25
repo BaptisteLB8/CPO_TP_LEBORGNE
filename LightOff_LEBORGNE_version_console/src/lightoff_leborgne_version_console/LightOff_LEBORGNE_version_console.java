ba49458bb2c2d2e0ccc31449fa67c8e326ea628c
bapti <bapti@LAPTOP-OARLVC8V.education.lakanal.epf.fr>
2023-10-25 09:11:15 +0200

    TP perso étape 5

---
+++ LightOff_LEBORGNE_version_console/src/lightoff_leborgne_version_console/LightOff_LEBORGNE_version_console.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_leborgne_version_console;
+
+/**
+ *
+ * @author bapti
+ */
+public class LightOff_LEBORGNE_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        CelluleLumineuse cellule1 = new CelluleLumineuse();
+        System.out.println("État initial de la cellule 1 : " + cellule1);
+        
+        cellule1.allumer();
+        System.out.println("État après avoir allumé la cellule 1 : " + cellule1);
+        cellule1.eteindre();
+        System.out.println("État après avoir éteint la cellule 1 : " + cellule1);
+    }
+}
+
+
+
